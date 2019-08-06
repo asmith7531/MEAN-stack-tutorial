e8fe1822ea32b30f9d77982802c010440b2f0f61
unknown <austinsmith7531@gmail.com>
2019-08-06 02:32:09 -0400

    added the expansion and the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatInputModule } from "@angular/material";
-import { MatCardModule } from "@angular/material";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/posts-lists/post-list.component";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatExpansionModule
+} from "@angular/material";
+
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
