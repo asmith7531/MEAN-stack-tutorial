9006db645a0680553f8c6b1f79dce10913d7e56e
unknown <austinsmith7531@gmail.com>
2019-08-06 06:30:42 -0400

    added form validation

--- src/app/posts/posts-lists/post-list.component.ts
+++ src/app/posts/posts-lists/post-list.component.ts
@@ -1,13 +1,10 @@
 import { Component, Input } from "@angular/core";
-
+import { Post } from "../post.model";
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent {
-  // { title: "first post", content: "this is the first post" },
-  // { title: "second post", content: "this is the second post" },
-  // { title: "third post", content: "this is the third post" }
   @Input() posts = [];
 }
